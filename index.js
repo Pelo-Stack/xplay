9c809eecb1427f3fcb0d6c9fb60038af9d93bdfc
Pelo-Stack <75405489+Pelo-Stack@users.noreply.github.com>
2020-12-26 10:09:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -56,28 +56,13 @@ const time = moment(t * 1000).format('DD/MM HH:mm:ss')
 
 
         switch(command) {
-        case '!sticker':
-        case '!stiker':
-            if (isMedia && type === 'image') {
-                const mediaData = await decryptMedia(message, uaOverride)
-                const imageBase64 = `data:${mimetype};base64,${mediaData.toString('base64')}`
-                await client.sendImageAsSticker(from, imageBase64)
-            } else if (quotedMsg && quotedMsg.type == 'image') {
-                const mediaData = await decryptMedia(quotedMsg, uaOverride)
-                const imageBase64 = `data:${quotedMsg.mimetype};base64,${mediaData.toString('base64')}`
-                await client.sendImageAsSticker(from, imageBase64)
-            } else if (args.length === 2) {
-                const url = args[1]
-                if (url.match(isUrl)) {
-                    await client.sendStickerfromUrl(from, url, { method: 'get' })
-                        .catch(err => console.log('Caught exception: ', err))
-                } else {
-                    client.reply(from, mess.error.Iv, id)
-                }
-            } else {
-                    client.reply(from, mess.error.St, id)
-            }
-            break
+        case 'sticker':
+           const image = await client.downloadAndSaveMediaMessage(m)
+           exec('cwebp -q 50 ' + image + ' -o temp/' + time + '.webp', (error, stdout, stderr) => {
+               let result = fs.readFileSync('temp/' + time + '.webp')
+               client.sendMessage(id, result, MessageType.sticker)
+           })
+           break
         case '!stickergif':
         case '!stikergif':
         case '!sgif':
